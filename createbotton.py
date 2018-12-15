53d728fa51dca738d8c1273c4419766c87c86ef8
liberbell <liberbell@gmail.com>
2018-12-15 17:13:58 +0900

    createbotton2

--- createbotton.py
+++ createbotton.py
@@ -4,5 +4,14 @@ from tkinter import ttk
 win = tk.Tk()
 win.title('Python GUI')
 # win.resizable(0,0)
-ttk.Label(win, text='A label').grid(column=0, row=0)
+# ttk.Label(win, text='A label').grid(column=0, row=0)
+
+alabel = ttk.Label(win, text='A label')
+alabel.grid(column=0, row=0)
+
+def clickMe():
+    action.configure(text='** I have been clicked! **')
+    alabel.configure(foreground='red')
+    alabel.configure(text='A Red Label')
+    
 win.mainloop()
