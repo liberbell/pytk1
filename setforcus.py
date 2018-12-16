e683f44d6d78656c60810281d3ee5b932ce0b767
liberbell <liberbell@gmail.com>
2018-12-16 12:38:25 +0900

    setforcus1

---
+++ setforcus.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import ttk
+
+win = tk.Tk()
+win.title('Python GUI')
+# win.resizable(0,0)
+# ttk.Label(win, text='A label').grid(column=0, row=0)
+
+alabel = ttk.Label(win, text='A label')
+alabel.grid(column=0, row=0)
+
+def clickMe():
+    action.configure(text='Hello ' + name.get())
+
+ttk.Label(win, text='Enter a name:').grid(column=0, row=0)
+
+name = tk.StringVar()
+nameEntered = ttk.Entry(win, width=12, textvariable=name)
+nameEntered.grid(column=0, row=1)
+
+action = ttk.Button(win, text='Click Me!', command=clickMe)
+action.grid(column=1, row=1)
+
+win.mainloop()
