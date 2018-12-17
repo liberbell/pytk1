ab8226b58d3a73d932a146c53f704e4486917ad5
liberbell <liberbell@gmail.com>
2018-12-18 07:57:52 +0900

    checkbar4

--- checkbar.py
+++ checkbar.py
@@ -47,7 +47,7 @@ numberChosen.grid(column=1, row=1)
 numberChosen.current(0)
 
 chVarDis = tk.IntVar()
-check1 = tk.Checkbutton(win, text='Disabled', variable=chVarUn)
+check1 = tk.Checkbutton(win, text='Disabled', variable=chVarDis, state='disabled')
 check1.select()
 check1.grid(column=0, row=4, sticky=tk.W)
 
@@ -57,7 +57,9 @@ check2.select()
 check2.grid(column=1, row=4, sticky=tk.W)
 
 chVarEn = tk.IntVar()
-check3 = tk.Checkbutton(win, text='')
+check3 = tk.Checkbutton(win, text='enabled', variable=)
+check3.select()
+check3.grid(column=2, row=4, sticky=tk.W)
 
 
 nameEntered.focus()      # Place cursor into name Entry
