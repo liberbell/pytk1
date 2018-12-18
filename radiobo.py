1dfdb13ba0a6ca11232cd9cf2c3a177afc169c38
liberbell <liberbell@gmail.com>
2018-12-19 08:52:06 +0900

    radiobo3

--- radiobo.py
+++ radiobo.py
@@ -3,4 +3,10 @@ COLOR2 = 'Gold'
 COLOR3 = 'Red'
 
 def radCall():
-    
+    radSel = radVar.get()
+    if radSel == 1:
+        win.configure(background=COLOR1)
+    elif radSel == 2:
+        win.configure(background=COLOR2)
+    elif radSel == 3:
+        win.configure(background=COLOR3)
