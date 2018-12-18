8954980ef2b168defa1d7297ae0776579978fb4b
liberbell <liberbell@gmail.com>
2018-12-19 08:56:56 +0900

    radiobo3

--- radiobo.py
+++ radiobo.py
@@ -1,3 +1,63 @@
+from tkinter as tk
+from tkinter import ttk
+from tkinter import scrolledtext
+
+mport tkinter as tk
+from tkinter import ttk
+
+# Create instance
+win = tk.Tk()
+
+# Add a title
+win.title("Python GUI")
+
+# Disable resizing the GUI
+#win.resizable(0,0)
+
+# Modify adding a Label
+aLabel = ttk.Label(win, text="A Label")
+aLabel.grid(column=0, row=0)
+
+# Modified Button Click Function
+def clickMe():
+    action.configure(text='Hello ' + name.get() + ' '+ numberChosen.get())
+
+# Changing our Label
+ttk.Label(win, text="Enter a name:").grid(column=0, row=0)
+
+# Adding a Textbox Entry widget
+name = tk.StringVar()
+nameEntered = ttk.Entry(win, width=12, textvariable=name)
+nameEntered.grid(column=0, row=1)
+
+# Adding a Button
+action = ttk.Button(win, text="Click Me!", command=clickMe)
+action.grid(column=2, row=1)
+#action.configure(state='disabled')    # Disable the Button Widget
+
+ttk.Label(win, text="Choose a number:").grid(column=1, row=0)
+number = tk.StringVar()
+numberChosen = ttk.Combobox(win, width=12, textvariable=number, state='readonly')
+numberChosen['values'] = (1, 2, 4, 42, 100)
+numberChosen.grid(column=1, row=1)
+numberChosen.current(0)
+
+chVarDis = tk.IntVar()
+check1 = tk.Checkbutton(win, text='Disabled', variable=chVarDis, state='disabled')
+check1.select()
+check1.grid(column=0, row=4, sticky=tk.W)
+
+chVarUn = tk.IntVar()
+check2 = tk.Checkbutton(win, text='Unchecked', variable=chVarUn)
+check2.select()
+check2.grid(column=1, row=4, sticky=tk.W)
+
+chVarEn = tk.IntVar()
+check3 = tk.Checkbutton(win, text='Enabled', variable=chVarEn)
+check3.select()
+check3.grid(column=2, row=4, sticky=tk.W)
+
+
 COLOR1 = 'Blue'
 COLOR2 = 'Gold'
 COLOR3 = 'Red'
@@ -10,3 +70,6 @@ def radCall():
         win.configure(background=COLOR2)
     elif radSel == 3:
         win.configure(background=COLOR3)
+
+radVar = tk.IntVar()
+rad1 = tk.Radiobotton
