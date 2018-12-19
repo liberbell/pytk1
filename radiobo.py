ee91162ff754814c7480bfc9bb60859b18bb03dc
liberbell <liberbell@gmail.com>
2018-12-19 09:02:39 +0900

    radiobo7

--- radiobo.py
+++ radiobo.py
@@ -72,6 +72,7 @@ def radCall():
         win.configure(background=COLOR3)
 
 radVar = tk.IntVar()
+
 rad1 = tk.Radiobotton(win, text=COLOR1, variable=radVar, value=1, command=radCall)
 rad1.grid(column=0, row=5, sticky=tk.W, columnspan=3)
 
