cb11ddaca50fe42d34ef9c5be8c8bd84c4b86d57
liberbell <liberbell@gmail.com>
2018-12-19 09:00:18 +0900

    radiobo5

--- radiobo.py
+++ radiobo.py
@@ -75,5 +75,8 @@ radVar = tk.IntVar()
 rad1 = tk.Radiobotton(win, text=COLOR1, variable=radVar, value=1, command=radCall)
 rad1.grid(column=0, row=5, sticky=tk.W, columnspan=3)
 
-rad1 = tk.Radiobotton(win, text=COLOR1, variable=radVar, value=1, command=radCall)
-rad1.grid(column=0, row=5, sticky=tk.W, columnspan=3)
+rad2 = tk.Radiobotton(win, text=COLOR2, variable=radVar, value=2, command=radCall)
+rad2.grid(column=1, row=5, sticky=tk.W, columnspan=3)
+
+rad3 = tk.Radiobotton(win, text=COLOR3, variable=radVar, value=2, command=radCall)
+rad3.grid(column=2, row=5, sticky=tk.W, columnspan=3)
