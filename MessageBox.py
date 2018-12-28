639f81556f3fd12296dd985fb9d7ac11a5a88a1d
liberbell <liberbell@gmail.com>
2018-12-28 14:57:21 +0900

    MessageBox6

--- MessageBox.py
+++ MessageBox.py
@@ -120,7 +120,7 @@ menuBar.add_cascade(label="File", menu=fileMenu)
 def _msgBox():
     # mBox.showinfo('Python Message Info Box', 'A Python GUI created using tkinter:\nThe year is 2018.')
     # mBox.showwarning('Python Message Info Box', 'A Python GUI created using tkinter:\nThe year is 2018.')
-    mBox.showerror('Python Error Message Box', 'A Python created using tkinter:\nError:')
+    mBox.showerror('Python Error Message Box', 'A Python created using tkinter:\nError: Houston ~ we Do have a serious problem.')
 
 # Add another Menu to the Menu Bar and an item
 helpMenu = Menu(menuBar, tearoff=0)
