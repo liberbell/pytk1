d04dd51f8a2448281c5783714bc1b8bb58cd0230
liberbell <liberbell@gmail.com>
2018-12-28 15:08:39 +0900

    independentbox4

--- independentbox.py
+++ independentbox.py
@@ -1,3 +1,3 @@
 from tkinter import messagebox as mBox
 
-mBox.showinfo('Python GUI created using tkinter:\nThe year is 2018')
+mBox.showinfo('', 'Python GUI created using tkinter:\nThe year is 2018')
