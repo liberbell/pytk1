c8ed37d437b2bc058028bcf1a976e437afb55579
liberbell <liberbell@gmail.com>
2018-12-30 22:33:31 +0900

    stringvar8

--- stringvar.py
+++ stringvar.py
@@ -2,11 +2,11 @@ import tkinter as tk
 
 win = tk.Tk()
 
-strData = tk.StringVar()
-
-strData.set('Hello StringVar')
-
-varData = strData.get()
+# strData = tk.StringVar()
+#
+# strData.set('Hello StringVar')
+#
+# varData = strData.get()
 
 intData = tk.IntVar()
 # print(varData)
